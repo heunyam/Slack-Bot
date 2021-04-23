2cddf9959e4410484363dbb6448cad29c203dcb1
heunyam <tpwns072@gmail.com>
2021-04-23 09:52:46 +0900

    fix: request mock and feat: MockResponse

--- tests/test_get_json.py
+++ tests/test_get_json.py
@@ -3,8 +3,19 @@ from pytest_mock import MockerFixture
 from config import BOT_OAUTH_ACCESS_TOKEN
 
 
-def test_get_channels_data(mocker: MockerFixture):
-    mocker.patch('src.get_channel_id.requests')
+class MockResponse:
+    def __init__(self, json_data):
+        self.json_data = json_data
+
+    def json(self):
+        return self.json_data
+
+
+def test_get_channels_data(mocker: MockerFixture) -> None:
+    mock_json_data = {
+        'ok': True
+    }
+    mocker.patch('src.get_channel_id.requests.get', return_value=MockResponse(mock_json_data))
 
     result = get_channels_data(BOT_OAUTH_ACCESS_TOKEN)
-    assert result['ok']
+    assert result['ok'] is True
