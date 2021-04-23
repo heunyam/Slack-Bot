6cd8173bb1cea7b40586e1ca41a39ee447d693b2
heunyam <tpwns072@gmail.com>
2021-04-23 09:57:08 +0900

    feat: Add get_channel_id

--- src/get_channel_id.py
+++ src/get_channel_id.py
@@ -1,12 +1,26 @@
 import requests
 
 
-def get_channels_data(access_token) -> dict:
+def get_channels_data(token) -> dict:
     url = 'https://slack.com/api/conversations.list'
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Bearer ' + access_token
+        'Authorization': 'Bearer ' + token
     }
 
     res = requests.get(url=url, headers=headers)
     return res.json()
+
+
+def get_channels(token) -> list:
+    data = get_channels_data(token)
+
+    return data.get('channels')
+
+
+def get_channel_id(channel_name, token) -> str:
+    channels = get_channels(token)
+
+    for channel in channels:
+        if channel['name'] == channel_name:
+            return channel['id']
