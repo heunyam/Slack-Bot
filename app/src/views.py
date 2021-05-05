638c43d6fbc541ce3348db1c09775ee7defbfb6b
heunyam <tpwns072@gmail.com>
2021-05-05 17:26:07 +0900

    feat: Add '/' PingAPI, '/hello' HelloAPI

--- app/src/views.py
+++ app/src/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import Response, APIView
+import os
 
-# Create your views here.
+
+class HelloAPI(APIView):
+    def get(self, *args, **kwargs):
+        return Response({
+            'hello': os.environ['SLACK_BOT_ACCESS_TOKEN']
+        })
+
+
+class PingAPI(APIView):
+    def get(self, *args, **kwargs):
+        return Response({
+            'ping': 'pong'
+        })
