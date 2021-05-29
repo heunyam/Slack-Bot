950c676942a9b63d92aec67d254064021535ca36
heunyam <tpwns072@gmail.com>
2021-05-29 23:38:06 +0900

    refactor: get today commit 함수로 분리

--- app/utils/get_commit_events.py
+++ app/utils/get_commit_events.py
@@ -1,22 +1,21 @@
 from datetime import datetime, timedelta
 from github import Github
 
-today = datetime.today()
-today_date = datetime(today.year, today.month, today.day)
-today_date_ko = today_date - timedelta(hours=9)
-one_week_ago_ko = today_date_ko - timedelta(days=7)
 
-username = 'heunyam'
-password = 'sejun0702!'
+def get_today_commit_events(username, password):
+    today = datetime.today()
+    today_date = datetime(today.year, today.month, today.day)
+    today_date_ko = today_date - timedelta(hours=9)
 
-client = Github(username, password)
+    client = Github(username, password)
+    commit_events = []
 
-commit_events = []
+    for event in client.get_user(username).get_events():
+        if event.created_at > today_date_ko:
+            if event.type in ['PushEvent', 'PullRequestEvent']:
+                commit_events.append(event)
 
-for event in client.get_user(username).get_events():
-    if event.created_at > today_date_ko:
-        if event.type in ['PushEvent', 'PullRequestEvent']:
-            commit_events.append(event)
+        else:
+            break
 
-    else:
-        break
+    return commit_events
