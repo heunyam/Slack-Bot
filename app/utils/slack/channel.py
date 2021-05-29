b4102909b9f4f636e99cddc15a514039c6d3ffc6
heunyam <tpwns072@gmail.com>
2021-05-30 01:37:20 +0900

    refactor: app.utils.slack 으로 이동

---
+++ app/utils/slack/channel.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def get_channels_data(token) -> dict:
+    url = 'https://slack.com/api/conversations.list'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer ' + token
+    }
+
+    res = requests.get(url=url, headers=headers)
+    return res.json()
+
+def get_channels(token) -> list:
+    data = get_channels_data(token)
+
+    return data.get('channels')
+
+
+def get_channel_id(channel_name, token) -> str:
+    channels = get_channels(token)
+
+    for channel in channels:
+        if channel['name'] == channel_name:
+            return channel['id']
