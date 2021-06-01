fc4d4d8cc90b9bb3c238f9dd447a8e342c921bd2
heunyam <tpwns072@gmail.com>
2021-06-01 23:36:12 +0900

    fix: user_id, password 대신 github token을 사용

--- app/utils/github.py
+++ app/utils/github.py
@@ -1,16 +1,18 @@
 from datetime import datetime, timedelta
 from github import Github
+import os
 
 
-def get_today_commit_events(username, password):
+def get_today_commit_events(user_id):
     today = datetime.today()
     today_date = datetime(today.year, today.month, today.day)
     today_date_ko = today_date - timedelta(hours=9)
 
-    client = Github(username, password)
+    client = Github(login_or_token=os.getenv('GITHUB_TOKEN'))
     commit_events = []
+    events = client.get_user(user_id).get_events()
 
-    for event in client.get_user(username).get_events():
+    for event in events:
         if event.created_at > today_date_ko:
             if event.type in ['PushEvent', 'PullRequestEvent']:
                 commit_events.append(event)
