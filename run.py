4f75af0613c0debeb1121918b09c21f34df1fd7b
heunyam <tpwns072@gmail.com>
2021-06-01 23:02:15 +0900

    feat: 앱 실행시 config 설정

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app import create_app
+from config.app import set_config
 from app.models import *
 
 
 app = create_app()
 
 if __name__ == '__main__':
+    set_config()
     init_db()
     app.run()
