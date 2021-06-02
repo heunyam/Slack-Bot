1897cb217cf38245fea838758c3a55ae2dbda811
heunyam <tpwns072@gmail.com>
2021-06-02 23:32:28 +0900

    refactor: api 이름을 SlackEventAPI로 변경

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,11 +1,11 @@
 from flask_restful import Api
 from flask import Blueprint
 
-from app.controllers.slack_event import HelloAPI
+from app.controllers.slack_event import SlackEventAPI
 from app.controllers.user import UserAPI
 
 api_bp = Blueprint('api', __name__)
 
 api = Api(api_bp)
-api.add_resource(HelloAPI, '/')
+api.add_resource(SlackEventAPI, '/')
 api.add_resource(UserAPI, '/user')
